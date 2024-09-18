da7e04fd31bb3eaa1318247df72d7ada9d8c063f
Haeward <haewardy@gmail.com>
2024-09-18 22:05:01 +0800

    fix: modify font filepath

--- packages/excalidraw/fonts/index.ts
+++ packages/excalidraw/fonts/index.ts
@@ -23,8 +23,8 @@ import Excalifont from "./assets/Excalifont-Regular.woff2";
 import Cascadia from "./assets/CascadiaCode-Regular.woff2";
 import ComicShanns from "./assets/ComicShanns-Regular.woff2";
 import LiberationSans from "./assets/LiberationSans-Regular.woff2";
-import XiaolaiMonoSC from "../../public/XiaolaiMonoSC.ttf";
-import XiaolaiSCRegular from "../../public/XiaolaiSC-Regular.ttf";
+import XiaolaiMonoSC from "./aassets/XiaolaiMonoSC.ttf";
+import XiaolaiSCRegular from "./aassets/XiaolaiSC-Regular.ttf";
 
 import LilitaLatin from "https://fonts.gstatic.com/s/lilitaone/v15/i7dPIFZ9Zz-WBtRtedDbYEF8RXi4EwQ.woff2";
 import LilitaLatinExt from "https://fonts.gstatic.com/s/lilitaone/v15/i7dPIFZ9Zz-WBtRtedDbYE98RXi4EwSsbg.woff2";
